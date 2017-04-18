ca5dd60cb41063a8f386e6100f6542ac05e081f5
theprogrammingthinker <blackedition.me@gmail.com>
2017-04-19 01:31:22 +0600

    Create a field to store the weather display TextView

--- app/src/main/java/com/theprogrammingthinker/www/sunshine/MainActivity.java
+++ app/src/main/java/com/theprogrammingthinker/www/sunshine/MainActivity.java
@@ -2,12 +2,18 @@ package com.theprogrammingthinker.www.sunshine;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mWeatherTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
+
+        mWeatherTextView = (TextView) findViewById(R.id.tv_weather_data);
     }
 }
